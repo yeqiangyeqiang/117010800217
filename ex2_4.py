5527ba5896610cdf78cf79d3fb4a58a423669ffb
yeqiangyeqiang <43289087+yeqiangyeqiang@users.noreply.github.com>
2018-10-12 10:16:46 +0800

    Add files via upload

---
+++ ex2_4.py
@@ -0,0 +1,6 @@
+import turtle as t
+t.fd(100)
+t.seth(120)
+t.fd(100)
+t.seth(-120)
+t.fd(100)
---
+++ ex2_7.py
@@ -0,0 +1,20 @@
+import turtle as t
+t.seth(90)
+t.fd(150)
+for i in range(2):
+    t.right(120)
+    t.fd(150)
+t.seth(90)
+t.penup()
+t.fd(75)
+t.seth(180)
+t.fd(43.3)
+t.pendown()
+t.seth(30)
+t.fd(150)
+for i in range(2):
+    t.right(120)
+    t.fd(150)
+t.seth(30)
+t.penup()
+t.fd(50)
